8c0ea12b7d71712ec2f536478edc95125be2e3c3
Dmitry Vorotnikov <dimassoo.tv@gmail.com>
2021-12-18 20:22:51 +0300

    MPI: adding src from MPI assignments archive

---
+++ Assignment18_slave.c
@@ -0,0 +1,12 @@
+#include "mpi.h"
+int main(int argc, char **argv)
+{
+	int rank;
+	MPI_Comm intercomm;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_get_parent(&intercomm);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Send(&rank, 1, MPI_INT, 0, rank, intercomm);
+	MPI_Finalize();
+	return 0;
+}
