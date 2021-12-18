b8988d8de318c9765af47e40eb2425bfa4651bae
Dmitry Vorotnikov <dimassoo.tv@gmail.com>
2021-12-19 01:03:25 +0300

    Assignments 5 and 4 with time measurements

--- Assignment4.cpp
+++ Assignment4.cpp
@@ -5,10 +5,12 @@ int main(int argc, char* argv[]) {
 	MPI_Init(&argc, &argv);
 	int rank, n, i, message;
     int SIXTH = 6;
+    double start_t, fin_t;
     MPI_Status status;
     MPI_Comm_size(MPI_COMM_WORLD, &n);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 //    my version is 6, so, sixth process receives the messages
+    start_t = MPI_Wtime();
 	if (rank == SIXTH)
 	{
         cout << "Hello from process " << rank << '\n';
@@ -19,6 +21,8 @@ int main(int argc, char* argv[]) {
 	}
 //	other processes sends their ranks to p with rank = 6
 	else MPI_Send(&rank, 1, MPI_INT, SIXTH, 0, MPI_COMM_WORLD);
+	fin_t = MPI_Wtime();
+	cout << "Execution time for the process " << rank << "= " << (fin_t - start_t) << '\n';
 	MPI_Finalize();
 	return 0;
 }
--- Assignment5.cpp
+++ Assignment5.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "mpi.h"
+#include <mpi.h>
 #define NTIMES 100
 using namespace std;
 int main(int argc, char **argv)
@@ -10,10 +10,16 @@ int main(int argc, char **argv)
 	char *name = new char;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	// processor name: name is a unique name of a actual node, len is length of the name
 	MPI_Get_processor_name(name, &len);
+	// fix time
 	time_start = MPI_Wtime();
+	// 1
+	// fix time for each iteration
 	for (i = 0; i < NTIMES; i++)
 		time_finish = MPI_Wtime();
+	// 2
+	// find process exec time between 1 and 2 and print  average iteration time in std out
 	cout << "processor " << name << ", process " << rank << "time = " << (time_finish - time_start) / NTIMES << endl;
 	MPI_Finalize();
 }
