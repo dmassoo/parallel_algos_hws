d279641b302177e3bbb9591b507c104f5ce05907
Dmitry Vorotnikov <dimassoo.tv@gmail.com>
2021-12-23 23:41:08 +0300

    Assignment 18: version2 - adding third slave process + explanation comments

--- Assignment18_master.cpp
+++ Assignment18_master.cpp
@@ -2,17 +2,23 @@
 #include <mpi.h>
 int main(int argc, char **argv)
 {
-	int size, rank1, rank2;
+	int size, rank1, rank2, n_ps;
 	MPI_Status status;
+	//intercommunicator to communicate between groups
 	MPI_Comm intercomm;
 	char slave[200] = "/mnt/c/Users/dimas/CLionProjects/HW/parallel_algos_hws/slave.o";
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_spawn(slave, MPI_ARGV_NULL, 2, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercomm, MPI_ERRCODES_IGNORE);
+	//spawns 3 new processes with program name 'slave' with root 0
+	MPI_Comm_spawn(slave, MPI_ARGV_NULL, 3, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercomm, MPI_ERRCODES_IGNORE);
+	//receive from first two slaves their ranks
 	MPI_Recv(&rank1, 1, MPI_INT, 0, 0, intercomm, &status);
 	MPI_Recv(&rank2, 1, MPI_INT, 1, 1, intercomm, &status);
 	//Display "Slaves rank1 and rank2 are working", instead of the words rank1 and rank2 their values should be displayed.
 	std::cout << "Slaves " << rank1 << " and " <<  rank2 << " are working!" << '\n';
+	//receive number of slave processes from third slave
+	MPI_Recv(&n_ps, 1, MPI_INT, 2, 2, intercomm, &status);
+    std::cout << "Number of slaves = " << n_ps << '\n';
 	MPI_Finalize();
 	return 0;
 }
--- Assignment18_slave.cpp
+++ Assignment18_slave.cpp
@@ -2,12 +2,18 @@
 #include <mpi.h>
 int main(int argc, char **argv)
 {
-	int rank;
+	int rank, size;
 	MPI_Comm intercomm;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_get_parent(&intercomm);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Send(&rank, 1, MPI_INT, 0, rank, intercomm);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	//initial logic for first 2 slaves
+	if (rank <= 1)
+	    MPI_Send(&rank, 1, MPI_INT, 0, rank, intercomm);
+	//third slave sends num of processes
+	if (rank > 1)
+	    MPI_Send(&size, 1, MPI_INT, 0, rank, intercomm);
 	MPI_Finalize();
 	return 0;
 }
